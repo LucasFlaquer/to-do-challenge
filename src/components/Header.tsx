c28848ecad07405c32a3eaee4891b56af5a12a18
= <=>
2022-06-29 22:29:26 -0300

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
+import styles from './Header.module.scss'
 import Logo from '../assets/logo.svg'
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={Logo} alt="" />
     </header>
   )
